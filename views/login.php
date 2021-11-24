d5ee505405a32f3ce38613bcc3787901595ae604
Mathieu Morel <mathieu.morel@etu.uca.fr>
2021-11-24 17:03:33 +0100

    Log / Sign Up Views

---
+++ views/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Login - Sing Up</title>
+    </head>
+    <body>
+        <div class="">
+            <h3>Log in to ToutDouxListe</h3>
+            <form method="post" action="">
+                <p>
+                    <input type="email" name="mail" placeholder="Your email" required>
+                </p>
+                <p>
+                    <input type="text" name="password" placeholder="Password" required>
+                </p>
+                <button type="button" name="button">Log in</button>
+            </form>
+            <p>
+                Don't have an account?
+                <a href="signup.php">Sign up</a>
+            </p>
+        </div>
+    </body>
+</html>
---
+++ views/signup.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Login - Sing Up</title>
+    </head>
+
+    <body>
+        <div class="">
+            <h3>Want to register on ToutDouxListe?</h3>
+            <form method="post" action="">
+                <p>
+                    <input type="text" name="pseudo" placeholder="Your name" required>
+                </p>
+                <p>
+                    <input type="email" name="email" placeholder="Your email" required>
+                </p>
+                <p>
+                    <input type="text" name="password" placeholder="Password" required>
+                </p>
+                <button type="button" name="button">Sign up</button>
+            </form>
+            <p>
+                Already have an account?
+                <a href="login.php">Log in</a>
+            </p>
+        </div>
+    </body>
+
+</html>
